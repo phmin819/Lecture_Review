808cce5faa2b9ee7ec87e0718b945828963b37cc
박효민 <zeronin4094@gmail.com>
2026-04-20 21:12:16 +0900

    database-connect

--- backend/database.py
+++ backend/database.py
@@ -15,3 +15,5 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 def get_session():
     with Session(engine) as session:
         yield session
+
+        print("DB:", os.getenv("DATABASE_URL"))
